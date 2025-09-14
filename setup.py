c78ed4a166488b77ab72ed3340ff75ed1145435d
Pito Salas <pitosalas@gmail.com>
2025-09-14 15:07:52 -0400

    testing out the commands for the building the map

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/localization_launch.py']),
+        ('share/' + package_name + '/config', ['config/mapper_params_localization.yaml']),
     ],
     install_requires=['setuptools', 'click', 'prompt_toolkit'],
     zip_safe=True,
